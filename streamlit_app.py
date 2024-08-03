862a2666ed2f93c139fec05e1c66fb04a0045efd
AlzanAditya <135565539+AlzanAditya@users.noreply.github.com>
2024-08-03 22:01:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-
 # Membuat data dalam bentuk dictionary
 data = {
     'Kata': [
@@ -17,11 +16,24 @@ data = {
         'Tahun', 'Umur berapa', 'Ya', 'Tidak, bukan', 'Perkenalkan',
         'Berasal dari', 'Salam kenal', 'Inggris', 'India',
         'Brasil', 'Jepang', 'Thailand', 'Jerman', 'Cina'
+    ],
+    'Romaji': [
+        'kyoushi', 'gakusei', 'kaishain', 'shain', 'ginkouin',
+        'isha', 'kenkyuusha', 'daigaku', 'byouin', 'dare / donata',
+        'sai', 'nansai / oikutsu', 'hai', 'iie', 'hajimemashite',
+        'kara kimashita', 'douzo yoroshiku onegaishimasu', 'Igirisu', 'Indo',
+        'Burajiru', 'Nihon', 'Tai', 'Doitsu', 'Chuugoku'
     ]
 }
 
 # Membuat DataFrame dari dictionary
 df = pd.DataFrame(data)
 
-# Menampilkan tabel menggunakan Streamlit
-st.table(df)
+# Opsi untuk menampilkan Romaji atau tidak
+show_romaji = st.checkbox('Tampilkan Romaji', value=True)
+
+# Menampilkan tabel dengan atau tanpa kolom Romaji
+if show_romaji:
+    st.table(df)
+else:
+    st.table(df.drop(columns=['Romaji']))
